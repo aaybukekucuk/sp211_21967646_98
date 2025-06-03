e56d3e9d44e75c8d35327825a03aa22766b741b2
aaybukekucuk <aaybukekucuk@gmail.com>
2025-06-03 19:05:43 +0300

    Fix package name and url in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='sp211_21967646_98/-aybukekucuk',
+    name='sp211_21967646_98',
     version='0.1.1',
     author='Aybuke Kucuk',
     author_email='aaybukekucuk@gmail.com',  
     description='Kısa yol algoritmalarını içeren örnek Python paketi',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/aybukekucuk/shortestpath-aybukekucuk',  
+    url='https://github.com/aaybukekucuk/sp211_21967646_98',  # GitHub repo adresin
     packages=find_packages(),
     install_requires=[
         'numpy>=1.21.0',
